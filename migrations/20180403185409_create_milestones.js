f538eb8990db1284e460388e6196c5640ea3fec5
Thomas Saunders <thomas130@fastmail.com>
2018-04-03 20:44:38 +0000

    database migration: add 'milestones' table

---
+++ migrations/20180403185409_create_milestones.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('milestones', function(table){
+      table.string('description');
+      table.date('date_achieved');
+      table.increments('id').primary();
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('milestones')
+  ])
+};
